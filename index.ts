bab3117844dbfa3db2678b0e7110da99da065252
Roman-Ojha <80277272+roman-ojha@users.noreply.github.com>
2025-04-02 19:11:15 +0545

    feat: added getWeatherDataByCityName tool

--- index.ts
+++ index.ts
@@ -1,8 +1,69 @@
 // https://modelcontextprotocol.io/introduction
 
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import { z } from "zod";
 
 const server = new McpServer({
   name: "Weather Data MCP Server",
   version: "1.0.0",
 });
+
+// server.prompt // we can give a prompt to the user
+// server.resource // we can give a resource to the user
+
+// function that will get weather by city
+async function getWeatherDataByCityName(city: string) {
+  // for this example we will not going to fetch data from api
+  if (city.toLowerCase() === "kathmandu") {
+    return {
+      temp: "30C",
+      forecast: "sunny",
+    };
+  }
+  if (city.toLowerCase() === "lalitpur") {
+    return {
+      temp: "25C",
+      forecast: "cloudy",
+    };
+  }
+
+  if (city.toLowerCase() === "damak") {
+    return {
+      temp: "20C",
+      forecast: "rainy",
+    };
+  }
+
+  return {
+    temp: null,
+    error: "Unable to get data",
+  };
+}
+
+// we can also give a tool to the user
+server.tool(
+  "getWeatherDataByCityName",
+  // give a specific name to the tool so that LLM will know which tool to use
+  {
+    // so this tool will take a city name as input and return the weather data for that city
+    city: z.string(),
+  },
+  async ({ city }) => {
+    // return bellow format
+    // content: [
+    //     {
+    //       type: "text",
+    //       text: "Failed to retrieve alerts data",
+    //     },
+    //   ],
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(await getWeatherDataByCityName(city)), // here we have to give data in the form of string
+        },
+      ],
+    };
+  }
+);
